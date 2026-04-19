adc2468712eda5b309b900e1586e94ae1b846e0e
Elizabeth <cee777578@gmail.com>
2026-04-19 03:18:13 -0400

    empresas listas menos boton entrar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,7 @@
 import { Routes } from '@angular/router';
+import { EditCompany } from './features/administrator/edit-company/edit-company';
+import { MyCompanies } from './features/administrator/my-companies/my-companies';
+import { RegisterCompany } from './features/administrator/register-company/register-company';
 import { Login } from './features/auth/login/login';
 import { Register } from './features/auth/register/register';
 import { HomePage } from './features/landing/home_page/home_page';
@@ -15,5 +18,17 @@ export const routes: Routes = [
   {
     path: 'login',
     component: Login,
+  },
+  {
+    path: 'administrator/my-companies',
+    component: MyCompanies,
+  },
+  {
+    path: 'administrator/register-company',
+    component: RegisterCompany,
+  },
+  {
+    path: 'administrator/edit-company/:nit',
+    component: EditCompany,
   },
 ];
--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +7,19 @@ import { RouterLink } from '@angular/router';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly router: Router,
+  ) {}
+
+  protected iniciarSesion(event: SubmitEvent): void {
+    event.preventDefault();
+
+    const rol = this.route.snapshot.queryParamMap.get('rol');
+
+    if (rol === 'administrador') {
+      void this.router.navigate(['/administrator/my-companies']);
+    }
+  }
+}
