03ab7b3fb60f6d76382fe781b99a05cae1e6c482
Elizabeth <cee777578@gmail.com>
2026-04-19 02:36:25 -0400

    vistas de auth y landing completas

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -9,8 +9,18 @@ import { RouterLink } from '@angular/router';
 })
 export class Navbar {
   protected readonly sesionIniciada = signal(false);
+  protected readonly menuLoginAbierto = signal(false);
+
+  protected alternarMenuLogin(): void {
+    this.menuLoginAbierto.update((abierto) => !abierto);
+  }
+
+  protected cerrarMenuLogin(): void {
+    this.menuLoginAbierto.set(false);
+  }
 
   protected cerrarSesion(): void {
     this.sesionIniciada.set(false);
+    this.cerrarMenuLogin();
   }
 }
