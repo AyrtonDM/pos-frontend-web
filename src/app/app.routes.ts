84bef1fd77649a33de2efa2569fb29104730183d
Capi03 <cinthiacarrasco321@gmail.com>
2026-04-19 21:33:35 -0400

    cambios del inventario

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { Branches } from './features/administrator/companies/sections/branches/p
 import { EditBranch } from './features/administrator/companies/sections/branches/pages/edit-branch/edit-branch';
 import { ViewBranch } from './features/administrator/companies/sections/branches/pages/view-branch/view-branch';
 import { Staff } from './features/administrator/companies/sections/branches/sections/staff/staff';
+import { Inventario } from './features/administrator/companies/sections/branches/sections/inventario/inventario';
 import { EditCategory } from './features/administrator/companies/sections/products/pages/categorias/edit-category/edit-category';
 import { EditProduct } from './features/administrator/companies/sections/products/pages/productos/edit-product/edit-product';
 import { Products } from './features/administrator/companies/sections/products/pages/productos/products';
@@ -66,8 +67,9 @@ export const routes: Routes = [
 /*  */    canActivate: [authGuard],
   },
   {
-    path: 'administrator/company/:id/branch/:branchId/staff',
-    component: Staff,
+    path: 'administrator/company/:id/branch/:branchId/inventario',
+    component: Inventario,
+    canActivate: [authGuard],
   },
   {
     path: 'administrator/company/:id/products',
