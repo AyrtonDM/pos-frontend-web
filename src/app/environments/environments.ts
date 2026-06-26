a2824fcdbd2164b504f31cd696862a8059732d8c
Capi03 <cinthiacarrasco321@gmail.com>
2026-06-26 13:55:04 -0400

    uwwu

--- src/app/environments/environments.ts
+++ src/app/environments/environments.ts
@@ -2,4 +2,14 @@
 export const environments = {
     production: false,
     apiBaseUrl: 'http://localhost:8000',
+    firebase: {
+        apiKey: "AIzaSyA-wTcvwf5gh8mtYFaYXN8VeY-kxYiQ8v8",
+        authDomain: "pos-si2.firebaseapp.com",
+        projectId: "pos-si2",
+        storageBucket: "pos-si2.firebasestorage.app",
+        messagingSenderId: "181449830908",
+        appId: "1:181449830908:web:eab5f979040782aa218279"
+    },
+    firebaseVapidKey: 'BJAcsjp2Tkhjbt4L-u8J_cWzEkn6Q1FU_BvgZYaK4Iaz62ubsvOB16iFF8ljWn2U-aaPuwYM3M25WtqvZCHRpTw'
+
 };
